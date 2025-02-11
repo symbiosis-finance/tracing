131ceeba4c8b1fb8a4f178ad0cb608e75944c7a3
Nikolay Bryskin <nikicat@users.noreply.github.com>
2025-02-11 17:18:25 +0200

    implement zap.Logger output

--- tracing.go
+++ tracing.go
@@ -23,6 +23,7 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func reverseLookupHostname(ctx context.Context) (rHost string, err error) {
@@ -38,6 +39,122 @@ func reverseLookupHostname(ctx context.Context) (rHost string, err error) {
 
 func attributes(attrs ...attribute.KeyValue) []attribute.KeyValue {
 	return attrs
+}
+
+type loggingSpanProcessor struct {
+	startLogger *zap.Logger
+	endLogger   *zap.Logger
+}
+
+var _ sdktrace.SpanProcessor = loggingSpanProcessor{}
+
+func newLoggingSpanProcessor(logger *zap.Logger) loggingSpanProcessor {
+	return loggingSpanProcessor{
+		startLogger: logger.WithOptions(zap.AddCallerSkip(2)),
+		endLogger:   logger.WithOptions(zap.AddCallerSkip(3)),
+	}
+}
+
+func getLogFields(ctx context.Context) []attribute.KeyValue {
+	if v := ctx.Value(ctxAttributeKey); v != nil {
+		return v.([]attribute.KeyValue)
+	}
+	return nil
+}
+
+func AddLogFields(ctx context.Context, fields ...attribute.KeyValue) context.Context {
+	return context.WithValue(ctx, ctxAttributeKey, append(getLogFields(ctx), fields...))
+}
+
+func (lsp loggingSpanProcessor) OnStart(ctx context.Context, s sdktrace.ReadWriteSpan) {
+	if fields := getLogFields(ctx); fields != nil {
+		s.SetAttributes(append(fields, s.Attributes()...)...)
+	}
+	lsp.startLogger.Debug("start "+s.Name(), spanToZapFields(s)...)
+}
+
+type zapClock struct {
+	t time.Time
+}
+
+var _ zapcore.Clock = zapClock{}
+
+func (zc zapClock) Now() time.Time {
+	return zc.t
+}
+
+func (zc zapClock) NewTicker(d time.Duration) *time.Ticker {
+	return time.NewTicker(d)
+}
+
+func (lsp loggingSpanProcessor) OnEnd(s sdktrace.ReadOnlySpan) {
+	for _, ev := range s.Events() {
+		lsp.endLogger.WithOptions(zap.WithClock(zapClock{ev.Time})).Debug(ev.Name, eventToZapFields(s, ev)...)
+	}
+	lsp.endLogger.Debug("end "+s.Name(), spanToZapFields(s)...)
+}
+
+func (lsp loggingSpanProcessor) Shutdown(ctx context.Context) error   { return nil }
+func (lsp loggingSpanProcessor) ForceFlush(ctx context.Context) error { return nil }
+
+func attributeToZapField(attr attribute.KeyValue) zap.Field {
+	key := string(attr.Key)
+	value := attr.Value
+	switch value.Type() {
+	case attribute.BOOL:
+		return zap.Bool(key, value.AsBool())
+	case attribute.INT64:
+		return zap.Int64(key, value.AsInt64())
+	case attribute.FLOAT64:
+		return zap.Float64(key, value.AsFloat64())
+	case attribute.STRING:
+		return zap.String(key, value.AsString())
+	case attribute.BOOLSLICE:
+		return zap.Bools(key, value.AsBoolSlice())
+	case attribute.INT64SLICE:
+		return zap.Int64s(key, value.AsInt64Slice())
+	case attribute.FLOAT64SLICE:
+		return zap.Float64s(key, value.AsFloat64Slice())
+	case attribute.STRINGSLICE:
+		return zap.Strings(key, value.AsStringSlice())
+	default:
+		panic(fmt.Errorf("invalid type %d", value.Type()))
+	}
+}
+
+func attributesToZapFields(attrs []attribute.KeyValue) (fields []zap.Field) {
+	for _, attr := range attrs {
+		fields = append(fields, attributeToZapField(attr))
+	}
+	return
+}
+
+type ctxAttribute struct{}
+
+var ctxAttributeKey ctxAttribute
+
+func spanContextToZapFields(spanCtx trace.SpanContext, prefix string) []zap.Field {
+	return []zap.Field{
+		zap.Stringer(prefix+".traceID", spanCtx.TraceID()),
+		zap.Stringer(prefix+".ID", spanCtx.SpanID()),
+	}
+}
+
+func spanToZapFields(s sdktrace.ReadOnlySpan) []zap.Field {
+	fields := spanContextToZapFields(s.SpanContext(), "span")
+	if s.Status().Code == codes.Error {
+		fields = append(fields, zap.String("span.error", s.Status().Description))
+	}
+	fields = append(fields, zap.Stringer("span.kind", s.SpanKind()))
+	fields = append(fields, spanContextToZapFields(s.Parent(), "parent")...)
+	return append(fields, attributesToZapFields(s.Attributes())...)
+}
+
+func eventToZapFields(s sdktrace.ReadOnlySpan, ev sdktrace.Event) (fields []zap.Field) {
+	fields = spanContextToZapFields(s.SpanContext(), "span")
+	fields = append(fields, attributesToZapFields(ev.Attributes)...)
+	fields = append(fields, attributesToZapFields(s.Attributes())...)
+	return
 }
 
 func newTraceProvider(ctx context.Context, exp sdktrace.SpanExporter, cfg TracerConfig, serviceName, version string, logger *zap.Logger) (tp *sdktrace.TracerProvider) {
@@ -57,10 +174,16 @@ func newTraceProvider(ctx context.Context, exp sdktrace.SpanExporter, cfg Tracer
 		resource.NewWithAttributes(semconv.SchemaURL, attrs...),
 	))
 
-	tp = sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exp),
+	options := []sdktrace.TracerProviderOption{
 		sdktrace.WithResource(r),
-	)
+	}
+	if exp != nil {
+		options = append(options, sdktrace.WithBatcher(exp))
+	}
+	if cfg.EnableLogs {
+		options = append(options, sdktrace.WithSpanProcessor(newLoggingSpanProcessor(logger)))
+	}
+	tp = sdktrace.NewTracerProvider(options...)
 	logger.Info("tracing initialized", zap.Any("config", cfg), zap.String("hostname", hostname))
 	return
 }
@@ -68,6 +191,7 @@ func newTraceProvider(ctx context.Context, exp sdktrace.SpanExporter, cfg Tracer
 type TracerConfig struct {
 	// Only one is used
 	EnableStdout bool
+	EnableLogs   bool
 	GrpcUrl      string
 	HttpUrl      string
 }
@@ -103,11 +227,9 @@ func InitTracer(ctx context.Context, cfg TracerConfig, serviceName, version stri
 		}
 		exp = tryerr.Must(otlptracehttp.New(ctx, options...))
 	}
-	if exp != nil {
-		tp := newTraceProvider(ctx, exp, cfg, serviceName, version, logger)
-		otel.SetTracerProvider(tp)
-		otel.SetTextMapPropagator(propagation.TraceContext{})
-	}
+	tp := newTraceProvider(ctx, exp, cfg, serviceName, version, logger)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
 }
 
 func ShutdownTracer() {
@@ -161,4 +283,21 @@ func isNil(i any) bool {
 	default:
 		return false
 	}
+}
+
+func StoreTrace(ctx context.Context) (m propagation.MapCarrier) {
+	m = make(propagation.MapCarrier)
+	otel.GetTextMapPropagator().Inject(ctx, m)
+	return
+}
+
+func LoadTrace(ctx context.Context, m propagation.MapCarrier) context.Context {
+	return otel.GetTextMapPropagator().Extract(ctx, m)
+}
+
+func ErrorAttr(key string, err error) (attr attribute.KeyValue) {
+	if err != nil {
+		return attribute.Key(key).String(err.Error())
+	}
+	return
 }
