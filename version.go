42580d8f963e9fd61d45e566bf3a19f2004a7af6
Nikolay Bryskin <nikicat@users.noreply.github.com>
2025-04-30 13:06:26 +0300

    feat: add VersionNotSet const

--- version.go
+++ version.go
@@ -1,3 +1,6 @@
 package tracing
 
-var Version string
+const VersionNotSet = "notset"
+
+// This var should be set via `go build -ldflags "-X=github.com/symbiosis-finance/tracing.Version=v1.2.3" ...`
+var Version string = VersionNotSet
