8d20a2fc90e4a15a718c2d8c0b3eab0ac76ce323
Ruslan <kros8925@gmail.com>
2026-06-25 13:46:59 +0300

    refactor: groupped keys in one namespace together

--- semconv.go
+++ semconv.go
@@ -8,38 +8,34 @@ import (
 	semconv "go.opentelemetry.io/otel/semconv/v1.30.0"
 )
 
-const ChainIDKey = attribute.Key("chain.id")
+const (
+	ChainIDKey       = attribute.Key("chain.id")
+	ChainNameKey     = attribute.Key("chain.name")
+	BlockNumberKey   = attribute.Key("chain.block_number")
+	BlockHashKey     = attribute.Key("chain.block_hash")
+	TransactionIDKey = attribute.Key("chain.transaction_id")
+	LogIndexKey      = attribute.Key("chain.log_index") // log index in block
+)
 
 func ChainID(chainID int64) attribute.KeyValue {
 	return ChainIDKey.Int64(chainID)
 }
 
-const ChainNameKey = attribute.Key("chain.name")
-
 func ChainName(chainName string) attribute.KeyValue {
 	return ChainNameKey.String(chainName)
 }
-
-const BlockNumberKey = attribute.Key("chain.block_number")
 
 func BlockNumber(blockNumber uint64) attribute.KeyValue {
 	return BlockNumberKey.Int64(int64(blockNumber))
 }
 
-const BlockHashKey = attribute.Key("chain.block_hash")
-
 func BlockHash(blockHash fmt.Stringer) attribute.KeyValue {
 	return BlockHashKey.String(blockHash.String())
 }
 
-const TransactionIDKey = attribute.Key("chain.transaction_id")
-
 func TransactionID(txID fmt.Stringer) attribute.KeyValue {
 	return TransactionIDKey.String(txID.String())
 }
-
-// Log index in block
-const LogIndexKey = attribute.Key("chain.log_index")
 
 func LogIndex(index uint) attribute.KeyValue {
 	return LogIndexKey.Int(int(index))
